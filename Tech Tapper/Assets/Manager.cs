1379d2ab960dc0ee5df09c9a0883793c16b82f8c
Maximus Juul <Maximusjjuul@gmail.com>
2021-09-28 16:05:25 +0200

    Tilføjet bedre auto clicker

--- Tech Tapper/Assets/Manager.cs
+++ Tech Tapper/Assets/Manager.cs
@@ -7,16 +7,30 @@ public class Manager : MonoBehaviour
 {
     public Text ClicksTotalText;
     float TotalClicks;
+
+    // Default Auto click
     bool hasUpgrade;
     public int autoClicksPerSecond;
     public int minimumClicksToUnlockUpgrade;
+    //-------------------------------------------------------------------------------------------------------
 
+    // Keyboard Auto click
+    bool hasKeyboard;
+    public int keyboardClicksPerSecond;
+    public int minimumClicksToUnlockKeyboad;
+    //-------------------------------------------------------------------------------------------------------
+
+    // Default Click
     public void AddClicks()
     {
         TotalClicks++;
         ClicksTotalText.text = TotalClicks.ToString("0");
     }
+    //-------------------------------------------------------------------------------------------------------
 
+    // Auto Clickers
+
+    // Default Auto click
     public void AutoClickUpgrade()
     {
         if (!hasUpgrade && TotalClicks >= minimumClicksToUnlockUpgrade)
@@ -25,6 +39,19 @@ public class Manager : MonoBehaviour
             hasUpgrade = true;
         }
     }
+
+    // Keyboard Auto Click
+    public void AutoKeyboardUpgrade()
+    {
+        if (!hasKeyboard && TotalClicks >= minimumClicksToUnlockKeyboad)
+        {
+            TotalClicks -= minimumClicksToUnlockKeyboad;
+            hasKeyboard = true;
+        }
+    }
+    //-------------------------------------------------------------------------------------------------------
+
+    // Update
     private void Update()
     {
         if (hasUpgrade)
@@ -32,5 +59,12 @@ public class Manager : MonoBehaviour
             TotalClicks += autoClicksPerSecond * Time.deltaTime;
             ClicksTotalText.text = TotalClicks.ToString("0");
         }
+
+        if (hasKeyboard)
+        {
+            TotalClicks += keyboardClicksPerSecond * Time.deltaTime;
+            ClicksTotalText.text = TotalClicks.ToString("0");
+        }
     }
+
 }
