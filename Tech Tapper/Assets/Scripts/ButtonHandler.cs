f8bebe6c4625225aaae6a0c645a8be5af906ebd6
niko391a <69769473+niko391a@users.noreply.github.com>
2021-10-26 15:51:41 +0200

    1.1

--- Tech Tapper/Assets/Scripts/ButtonHandler.cs
+++ Tech Tapper/Assets/Scripts/ButtonHandler.cs
@@ -11,36 +11,44 @@ public class ButtonHandler : MonoBehaviour
     public void whenMainButtonClicked()
     {
         if (Main.activeInHierarchy == false)
-
+        {
+            Shop.SetActive(false);
             Main.SetActive(true);
-
+            Settings.SetActive(false);
+        }
         else
-
-            Main.SetActive(false);
-
+        {
+            Settings.SetActive(false);
+            //Main.SetActive(false);
+            Shop.SetActive(false);
+        }
     }
 
     public void whenSettingsButtonClicked()
     {
         if (Settings.activeInHierarchy == false)
-
+        {
+            Shop.SetActive(false);
+            //Main.SetActive(false);
             Settings.SetActive(true);
-
-        else
-
-            Settings.SetActive(false);
-
+        }
+        //else
+        //{
+        //    Settings.SetActive(false);
+        //}
     }
 
     public void whenShopButtonClicked()
     {
         if (Shop.activeInHierarchy == false)
-
+        {
             Shop.SetActive(true);
-
-        else
-
-            Shop.SetActive(false);
-
+            //Main.SetActive(false);
+            Settings.SetActive(false);
+        }
+        //else
+        //{
+        //    Shop.SetActive(false);
+        //}
     }
 }
