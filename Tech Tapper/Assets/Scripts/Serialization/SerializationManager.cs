37a2f44d4cc5e131f1abeabec7386197eff1a6d4
Nikolaj Heuer Løjmand Carlson <niko052b@elev.tec.dk>
2021-10-26 15:45:09 +0200

    Missing the Debug.LogErrorFormat

---
+++ Tech Tapper/Assets/Scripts/Serialization/SerializationManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using static System.Net.Mime.MediaTypeNames;
+using 
+
+
+public class Se : MonoBehaviour
+{
+    public static bool Save(string saveName, object saveData) //takes in the saveName string and the object saveData, and returns a boolean to make sure that it was successful 
+    {
+        BinaryFormatter formatter = getBinaryFormatter();
+
+        if(!Directory.Exists(Application.persistentDataPath + "/saves")) //Defines the path for our save game
+        {
+            Directory.CreateDirectory(Application.persistentDataPath) + "/saves");
+
+        }
+
+        string path = Application.persistentDataPath + "/saves" + saveName + ".save"; //.save is the name of the file
+
+        FileStream file = File.Create(path);
+
+        formatter.Serialize(file, saveData);
+
+        file.Close();
+
+        return true;
+    }
+    public static bool Load(string path)
+    {
+        if (!File.Exists(path)) //Checks if the file doesn't exist
+        {
+            return null;
+        }
+
+        BinaryFormatter formatter = GetBinaryFormatter();
+
+        FileStream file = File.Open(path, FileMode.Open);
+
+
+        try //Tries to open the file and if it is successful it will then return it
+        {
+            object save = formatter.Deserialize(file);
+            file.Close();
+            return save;
+
+        }
+        catch
+        {
+            Debug.LogErrorFormat("Failed to load file at {0}", path);
+
+
+        }
+
+    }
+
+    public static BinaryFormatter GetBinaryFormatter()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        return formatter;
+
+    }
+
+
+}
